dedc84c66d783318bdb523809b255246bcac393d
Mickael <mickaeldesclaux@gmail.com>
2025-07-09 21:55:35 +0200

    Add tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Button} from "@material-tailwind/react";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,7 @@ function App() {
         <h1 className="text-3xl font-bold underline">
             Hello world!
         </h1>
+        <Button className='text-9xl' onClick={() => alert('clicked')}>Test</Button>
         <div className="card">
             <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
